b612c8a222a47ce2d6eada84b4d1a31b908de0cf
Parishi <117592741+ParishiB@users.noreply.github.com>
2024-04-01 16:53:03 +0530

    Update promise_polyfill.js

--- promise_polyfill.js
+++ promise_polyfill.js
@@ -1,3 +1,23 @@
 // Implement promise
+
+Promise.prototype.myPromise = function (cb) {
+  this.then(
+    function (result) {
+      console.log("Promise is resolved");
+      cb(null, result);
+    },
+    function (err) {
+      console.log("Promise is rejected");
+      cb(err, null);
+    }
+  );
+};
+
+
 // Polyfill for Promise.all
+
+
+
+
+
 // Polyfill for Promise.race
